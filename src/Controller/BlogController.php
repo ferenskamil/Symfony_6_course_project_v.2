ae9870bfd8718e2c5a188ee4e23b9658003232f1
Kamil <mr.kaam@gmail.com>
2023-09-19 16:53:20 +0200

    Feat: dependency injection in BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+
+    /** Na kursie opisali to jako wstrzykiwanie zależności.
+     * Wstrzyknęliśmy usługę ArticleRepository do kontorlera i możemy z niej korzystać */
+    public function __construct(
+        private ArticleRepository $articleRepository
+    )
+    {
+    }
+
     #[Route(path: '/articles' , name: 'blog-articles')]
     public function mainPage() : Response
     {
+        /**
+         * ->findAll() przepisałem z kursu */
+        $articles = $this->articleRepository->findAll();
+
         return new Response(content: '<h1>Tu będą wyświetlane artykuły</h1>');
     }
 }
