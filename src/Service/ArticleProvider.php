46071dfd75008c5fe0b79ffccdeb25a5f0a85175
Kamil <mr.kaam@gmail.com>
2023-09-21 12:32:24 +0200

    Feat: Display images in 'articles' page

--- src/Service/ArticleProvider.php
+++ src/Service/ArticleProvider.php
@@ -2,18 +2,30 @@
 
 namespace App\Service;
 
+use App\Service\ImageProvider;
+
 class ArticleProvider
 {
+    public function __construct(
+        private ImageProvider $imageProvider
+    )
+    {
+    }
+
     public function transformDataForTwig(array $articles) : array
     {
         $transformedData['articles'] = [];
 
         foreach ($articles as $article) {
+
+            $images = $article->getImages()->toArray();
+
             $transformedData['articles'][] = [
                 'title' => $article->getTitle(),
                 'content' => substr($article->getContent() , 0 , 30) . '...',
                 'link' => "article/{$article->getId()}",
-                'addedAt' => $article->getAddedAt()
+                'addedAt' => $article->getAddedAt(),
+                'images' => $this->imageProvider->transformDataForTwig($images)
             ];
         }
 
---
+++ src/Service/ImageProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Image;
+class ImageProvider
+{
+
+    public function transformDataForTwig(array $images) : array
+    {
+        $transformedData = [];
+        foreach ($images as $image) {
+
+            $transformedData[] = [
+                'title' => $image->getTitle(),
+                'path' => Image::PATH_TO_IMAGES . $image->getPath(),
+                'alt' => $image->getAlt()
+            ];
+        }
+
+        return $transformedData;
+    }
+
+}
